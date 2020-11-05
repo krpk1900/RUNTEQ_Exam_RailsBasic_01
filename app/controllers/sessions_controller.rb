628843fe19e04e849d842a79f43f10f402b549db
Terai Shogo <krpk1900@TerainoMacBook-Pro.local>
2020-11-05 17:36:42 +0900

    Fix all errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
@@ -17,4 +17,3 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 end
-
